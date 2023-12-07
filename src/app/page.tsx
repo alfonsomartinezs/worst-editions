8a7490bf7aa399bcd85f8562298b0c310fffa362
alphaandsew <alphaandsew@gmail.com>
2023-12-06 20:52:30 -0800

    add the

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
      <h1 className={styles.title}>
+      <span className={styles.the}>THE</span>
       <span className={`${styles.squishyLetter} ${styles.w}`}>W</span>
       {/* <span className={`${styles.squishyLetter} ${styles.o}`}>O</span> */}
       <span className={`${styles.squishyLetter} ${styles.o}`}>O</span>
@@ -12,7 +13,7 @@ export default function Home() {
       <span className={`${styles.squishyLetter} ${styles.s}`}>S</span>
       <span className={`${styles.squishyLetter} ${styles.t}`}>T</span>
       
-      <span className={styles.fullWord}>EDITIONS</span>
+      <span className={styles.fullWord}>EDI<wbr/>TIO<wbr/>NS</span>
       </h1>
     </main>
   )
